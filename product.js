eb1d2f656b5415b13987fd5aff5313cf8a42e20e
Tanisha Jain <tanishajain0895@gmail.com>
2025-10-27 13:57:51 +0530

    Initial commit - Smoothie Bliss website files added

---
+++ product.js
@@ -0,0 +1,31 @@
+const products = [
+    {
+      id: 1,
+      name: "Mango fruit smoothie",
+      price: 250,
+      image: "IMG-20250519-WA0003.jpg",
+      description: "Delicious mango smoothie with fresh mangoes and ice."
+    },
+    {
+      id: 2,
+      name: "Avocado fruit smoothie",
+      price: 250,
+      image: "IMG-20250519-WA0004.jpg",
+      description: "Creamy avocado smoothie full of nutrients."
+    },
+    {
+      id: 3,
+      name: "Dragon fruit smoothie",
+      price: 250,
+      image: "IMG-20250519-WA0005.jpg",
+      description: "Exotic dragon fruit smoothie, refreshing and healthy."
+    },
+    {
+      id: 4,
+      name: "Choco banana fruit smoothie",
+      price: 250,
+      image: "IMG-20250519-WA0006.jpg",
+      description: "Sweet chocolate and banana smoothie perfect for dessert."
+    }
+  ];
+  
